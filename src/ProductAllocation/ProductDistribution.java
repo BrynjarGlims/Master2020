f9a0db9b82afbc280bb7a6565d42adcef3f6cfe9
Brynjar Glimsdal <brynjag@ntnu.no>
2020-02-06 13:47:09 +0100

    missing split deliveries

--- src/ProductAllocation/ProductDistribution.java
+++ src/ProductAllocation/ProductDistribution.java
@@ -4,49 +4,128 @@ import DataFiles.Customer;
 import DataFiles.Data;
 import DataFiles.DataReader;
 import DataFiles.Order;
+import javafx.scene.shape.ArcTo;
 
-import java.util.Arrays;
+import java.util.*;
+import java.util.concurrent.ThreadLocalRandom;
 
 public class ProductDistribution {
 
 
     public double[][] productDistribution;
     public Data data;
-    public ProductDelivery[] productDeliveries;
+    public OrderDelivery[] orderDeliveries;
     public double[] volumePerPeriod;
 
     public ProductDistribution(Data data){
         this.data = data;
         productDistribution = new double[data.numPeriods][data.customers.length];
-        productDeliveries = new ProductDelivery[data.numDeliveries];
+        orderDeliveries = new OrderDelivery[data.numDeliveries];
         volumePerPeriod = new double[data.numPeriods];
 
     }
 
-    public void makeDistribution(Data data){
+    public void makeDistribution(){
+        distributeDividables();
+    }
+
+
+    private void distributeNonDividables(){
         for (Customer c : data.customers){
             int[] visitDaysCopy = new int[c.requiredVisitPeriod.length];
             System.arraycopy(c.requiredVisitPeriod, 0, visitDaysCopy, 0, c.requiredVisitPeriod.length);
-            for (Order p : c.nonDividableProducts){
-                System.out.println(c.customerNumber);
-                int chosenDay = getMinimumPeriod(visitDaysCopy);
-                productDistribution[getMinimumPeriod(c.requiredVisitPeriod)][c.customerID] += p.volume;
-                visitDaysCopy[chosenDay] = 0;
+            for (Order o : c.nonDividableProducts){
+                int chosenPeriod = getMinimumPeriod(visitDaysCopy);
+                productDistribution[chosenPeriod][c.customerID] += o.volume;
+                volumePerPeriod[chosenPeriod] += o.volume;
+                visitDaysCopy[chosenPeriod] = 0;
+
+                orderDeliveries[o.orderID] = new OrderDelivery(o, chosenPeriod, o.volume);
             }
         }
     }
 
-
-    private int getMinimumPeriod(int[] possibleDays){
-        int currentIndex = -1;
-        for (int i = 0 ; i < possibleDays.length ; i++){
-            if (possibleDays[i] == 1){
-                currentIndex = i;
-                break;
+    private void distributeDividables(){
+        for (Customer c : data.customers){
+            for (Order o : c.dividableProducts){
             }
         }
-        System.out.println(Arrays.toString(possibleDays));
-        if (currentIndex == -1){throw new IllegalStateException("customer has no valid visit days");}
+
+
+    }
+
+    private double[] splitDelivery(Order order){
+        int numSplits = ThreadLocalRandom.current().nextInt(order.minFrequency, order.maxFrequency + 1);
+        double[] volumeSplits = distributeVolume(order,numSplits, 10);
+
+
+        return volumeSplits;
+    }
+
+    private double[] distributeVolume(Order order, int numSplits, int numFractions){
+        int fractions = numSplits*numFractions;
+        Queue<Integer> splits = new ArrayDeque<>();
+        double sum = 0;
+        int randomNumber;
+        for (int i = 0 ; i < fractions ; i++){
+            randomNumber = ThreadLocalRandom.current().nextInt(10,100);
+            sum += randomNumber;
+            splits.add(randomNumber);
+        }
+        double[] distribution = new double[numSplits];
+        int tempFraction;
+        for (int i = 0 ; i < numSplits ; i++){
+            tempFraction = 0;
+            for (int j = 0 ; j < numFractions ; j++){
+                tempFraction += splits.remove();
+            }
+            distribution[i] = order.volume * (tempFraction/sum);
+        }
+    return distribution;
+    }
+
+    private int[] getValidTimeWindows(double[] volumes, Customer customer){
+        ArrayList<Integer> periods = new ArrayList<>();
+        for (int i = 0 ; i < customer.numberOfVisitPeriods ; i++) {
+            if (customer.requiredVisitPeriod[i] == 1) {
+                periods.add(i);
+            }
+        }
+        Collections.shuffle(periods);
+        int[] orderDeliveries = new int[volumes.length];
+        for (int i = 0 ; i < volumes.length ; i++){
+            orderDeliveries[i] = periods.get(i);
+            }
+        return orderDeliveries;
+        }
+
+
+
+//        int[] deliveryPeriods = new int[data.numPeriods];
+//        int[] visitDaysCopy = new int[customer.requiredVisitPeriod.length];
+//        System.arraycopy(customer.requiredVisitPeriod, 0, visitDaysCopy, 0, customer.requiredVisitPeriod.length);
+//        if (volumes.length == 1){
+//            deliveryPeriods[getMinimumPeriod(visitDaysCopy)] = 1;
+//        }
+//        else if (volumes.length == 2){
+//
+//        }
+//
+//
+//
+//        return new int[2];
+    }
+
+    private int getMinimumPeriod(int[] possibleDays){
+        List<Integer> validPeriods = new ArrayList<>();
+        for (int i = 0 ; i < possibleDays.length ; i++){
+            if (possibleDays[i] == 1){
+                validPeriods.add(i);
+            }
+        }
+        if (validPeriods.isEmpty()){throw new IllegalStateException("customer has no valid visit days");}
+        int currentIndex = validPeriods.get(ThreadLocalRandom.current().nextInt(validPeriods.size()));
+
 
         for (int i = 0 ; i < volumePerPeriod.length ; i++){
             if (possibleDays[i] == 0) {continue;}
@@ -62,16 +141,26 @@ public class ProductDistribution {
         Data data = DataReader.loadData();
 
         ProductDistribution pd = new ProductDistribution(data);
-        pd.makeDistribution(data);
+        pd.makeDistribution();
         for(double[] period : pd.productDistribution){
             double sum = 0;
-            for(double d : period){
-                sum += d;
+            for(double customer : period){
+                sum += customer;
             }
-            System.out.println(sum);
-            System.out.println("TEST PUSH");
         }
+        double[] a = pd.distributeVolume(data.customers[2].dividableProducts[0],
+                ThreadLocalRandom.current().nextInt(data.customers[2].dividableProducts[0].minFrequency, data.customers[2].dividableProducts[0].maxFrequency + 1), 3);
+        System.out.println(Arrays.toString(a));
+        double sum = 0;
+        for (double e : a){
+            sum += e;
+        }
+        System.out.println(sum);
+        System.out.println(data.customers[2].dividableProducts[0].volume);
+
+
     }
+
 
 
 }
