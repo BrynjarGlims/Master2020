5d67dec1dcb50e5ade434f2d74dd277b94f9dd32
Brynjar Glimsdal <brynjag@ntnu.no>
2020-02-14 13:22:48 +0100

    random chromosome construction implemented

--- src/Individual/GiantTour.java
+++ src/Individual/GiantTour.java
@@ -1,22 +1,64 @@
 package Individual;
 
+import DataFiles.Customer;
+import DataFiles.Data;
+import DataFiles.DataReader;
+import DataFiles.VehicleType;
+
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.concurrent.ThreadLocalRandom;
 
 public class GiantTour {
 
-    ArrayList<Integer> chromosome;
+
+    ArrayList[][] chromosome;
 
     public GiantTour(){
-        this.chromosome = new ArrayList<Integer>();
-        this.chromosome.add(2);
-        this.chromosome.add(5);
-        this.chromosome.add(6);
-        this.chromosome.add(1);
-        this.chromosome.add(4);
-        this.chromosome.add(9);
-        this.chromosome.add(8);
     }
 
 
 
+    public void initializeGiantTour(Data data) {
+        constructChromosome(data.numberOfPeriods, data.numberOfVehicleTypes);
+        for (Customer c : data.customers) {
+            placeCustomer(c);
+        }
+        shuffleChromosome();
+    }
+
+    private void constructChromosome(int numPeriods, int numVehicleTypes){
+        chromosome = new ArrayList[numPeriods][numVehicleTypes];
+        for (int p = 0 ; p < numPeriods ; p++){
+            for (int vt = 0 ; vt < numVehicleTypes ; vt++){
+                chromosome[p][vt] = new ArrayList();
+            }
+        }
+    }
+
+    private void placeCustomer(Customer customer){
+        for (int i = 0 ; i < customer.requiredVisitPeriod.length ; i++){
+            if (customer.requiredVisitPeriod[i] == 1) {
+                chromosome[i][ThreadLocalRandom.current().nextInt(0, chromosome[0].length)].add(customer.customerID);
+            }
+        }
+    }
+
+    private void shuffleChromosome(){
+        for (int p = 0; p < chromosome.length; p++) {
+            for (int vt = 0; vt < chromosome[0].length; vt++) {
+                Collections.shuffle(chromosome[p][vt]);
+                System.out.println(chromosome[p][vt]);
+            }
+        }
+    }
+
+
+
+    public static void main(String[] args){
+        Data data = DataReader.loadData();
+        GiantTour gt = new GiantTour();
+        gt.initializeGiantTour(data);
+    }
 }
