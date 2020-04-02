649a5783ac883fb5a86a1299532812e796e4ee24
Brynjar Glimsdal <brynjar.glimsdal@gmail.com>
2020-04-02 14:54:35 +0200

    added multiple run option for MIP and changed main

--- src/main/java/Master2020/DataFiles/Parameters.java
+++ src/main/java/Master2020/DataFiles/Parameters.java
@@ -12,7 +12,8 @@ public class Parameters {
     public static final String timeWindowsFilePath = "data/Time_windows.csv";
     public static final String vehicleFilePath = "data/Vehicles.csv";
     public static final boolean doRandomSeed = true;
-    public static final long randomSeedValue = 7;
+    public static final int samples = 5;
+    public static long randomSeedValue = 1;
 
 
     //Master2020.Population parameters
@@ -24,7 +25,7 @@ public class Parameters {
     public static final int initialPopulationSize = 50;
     public static final int initialOrderDistributionPopulationSize = 50;
     public static final int maxNumberIterationsWithoutImprovement = 10;
-    public static final int maxNumberOfGenerations = 10;
+    public static final int maxNumberOfGenerations = 2;
 
     public static final double minimumFitnessDifferenceForClones = 10; //minimum fitness difference to encounter two individuals as clones
 
@@ -35,7 +36,7 @@ public class Parameters {
     //Loading data parameters
 
     public static final double distanceCutOffFromDepot = 3.5;
-    public static final int numberOfCustomers = 8; // A maximum of 76
+    public static final int numberOfCustomers = 3; // A maximum of 76
     public static final int numberOfVehicles = 5;
 
     
@@ -102,12 +103,12 @@ public class Parameters {
     public static final int upperBoundQuantity = 100;  // upper bound q variable
     public static final int upperBoundOvertime = 1000;  // upper bound qO variable
 
-    public static final boolean verboseArcFlow = true;
+    public static final boolean verboseArcFlow = false;
     public static final boolean verbosePathFlow = false;
     public static final boolean verboseJourneyBased = false;
 
     //PLOT PARAMETERS
-    public static final boolean savePlots = true;
+    public static final boolean savePlots = false;
     public static final boolean verbose = false;
     public static final char separator = ';';
 }
