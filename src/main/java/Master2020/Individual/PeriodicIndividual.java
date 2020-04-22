cded3b96dd07accb960bf07f773e12bcac1a4689
LarsTv <lars-tvinnereim@hotmail.com>
2020-04-22 15:36:27 +0200

    add before store

--- src/main/java/Master2020/Individual/PeriodicIndividual.java
+++ src/main/java/Master2020/Individual/PeriodicIndividual.java
@@ -2,11 +2,13 @@ package Master2020.Individual;
 
 import Master2020.DataFiles.Data;
 import Master2020.Population.PeriodicPopulation;
+import gurobi.GRB;
 
 public class PeriodicIndividual {
 
     public Data data;
     public Individual[] individuals;
+    public double fitness = Double.MAX_VALUE;
 
 
     public PeriodicIndividual(Data data){
@@ -27,5 +29,13 @@ public class PeriodicIndividual {
         }
     }
 
+    public double getFitness() {
+        this.fitness = 0;
+        for (int p = 0; p < data.numberOfPeriods; p++){
+            this.fitness += this.individuals[p].getFitness(true);
+        }
 
+
+        return
+    }
 }
