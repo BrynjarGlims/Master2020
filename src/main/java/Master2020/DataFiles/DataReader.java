3aac1f61c8adf6d9c8e66b4a550ea42f0da2eee1
Lars Kåre Lillestøl Tvinnereim <lktvinne@ntnu.no>
2020-05-22 15:33:17 +0200

    removed invalid orders

--- src/main/java/Master2020/DataFiles/DataReader.java
+++ src/main/java/Master2020/DataFiles/DataReader.java
@@ -50,14 +50,17 @@ public class DataReader {
                     customerID++;
                 }
             }
-            productList.add(new Order(productID, customerID,
-                    Double.parseDouble(productData.get(line)[12]),
-                    checkSplitAttribute(productData.get(line)[6], line),
-                    productData.get(line)[3],
-                    Integer.parseInt(productData.get(line)[7]),
-                    Integer.parseInt(productData.get(line)[8]),
-                    Integer.parseInt(productData.get(line)[9])));
-            productID++;
+            if (Double.parseDouble(productData.get(line)[12]) > 0){
+                productList.add(new Order(productID, customerID,
+                        Double.parseDouble(productData.get(line)[12]),
+                        checkSplitAttribute(productData.get(line)[6], line),
+                        productData.get(line)[3],
+                        Integer.parseInt(productData.get(line)[7]),
+                        Integer.parseInt(productData.get(line)[8]),
+                        Integer.parseInt(productData.get(line)[9])));
+                productID++;
+            }
+
         }
         return convertCustomerList(customerList);
     }
