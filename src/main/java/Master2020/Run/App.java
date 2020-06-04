a0859a1c109a62137ca84009fbc582c7f3acdb21
LarsTv <lars-tvinnereim@hotmail.com>
2020-06-04 15:35:19 +0200

    run on server test

--- src/main/java/Master2020/Run/App.java
+++ src/main/java/Master2020/Run/App.java
@@ -19,7 +19,7 @@ public class App {
 
 
     public static void main(String[] args) throws Exception {
-        //args = new String[]{"HYBRID", "debug"};
+        args = new String[]{"HYBRID", "debug"};
         switch (args[1]) {
             case "base":
                 baseCase(args);
