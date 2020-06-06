063608297dbbf4deebcde3a66ca6f064f15f373c
Brynjar Glimsdal <brynjar.glimsdal@gmail.com>
2020-06-07 01:50:08 +0200

    journeys adjusted for instance size

--- src/main/java/Master2020/Run/App.java
+++ src/main/java/Master2020/Run/App.java
@@ -15,6 +15,7 @@ public class App {
     private static int[][][] seeds;//dataset, instance size, seed
     private static int[] customers;//instance size
     private static int[] vehicles;//instance size
+    private static int[] journeys;//instance size
 
 
     public static void main(String[] args) throws Exception {
@@ -140,30 +141,36 @@ public class App {
         seeds = new int[2][5][];
         customers = new int[5];
         vehicles = new int[5];
+        journeys = new int[5];
         //25
-        seeds[0][0] = new int[]{};//15, 84};
-        seeds[1][0] = new int[]{};//69,85};
+        seeds[0][0] = new int[]{15, 84};
+        seeds[1][0] = new int[]{69,85};
         customers[0] = 25;
         vehicles[0] = 12;
+        journeys[0] = 3;
         //50
-        seeds[0][1] = new int[]{};//94,41};
-        seeds[1][1] = new int[]{};//20,60};
+        seeds[0][1] = new int[]{94,41};
+        seeds[1][1] = new int[]{20,60};
         customers[1] = 50;
         vehicles[1] = 25;
+        journeys[1] = 3;
         //75
-        seeds[0][2] = new int[]{}; //32,18,37};
-        seeds[1][2] = new int[]{}; //1};
+        seeds[0][2] = new int[]{32,18,37};
+        seeds[1][2] = new int[]{1};
         customers[2] = 75;
         vehicles[2] = 32;
+        journeys[2] = 2;
         //100
-        seeds[0][3] = new int[]{}; //97,46,35,76};
+        seeds[0][3] = new int[]{97,46,35,76};
         seeds[1][3] = new int[]{};
         customers[3] = 100;
         vehicles[3] = 50;
+        journeys[3] = 2;
         //115
         seeds[0][4] = new int[]{10,27};
         seeds[1][4] = new int[]{};
         customers[4] = 115;
         vehicles[4] = 62;
+        journeys[4] = 2;
     }
 }
