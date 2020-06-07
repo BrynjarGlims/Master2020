3b50798b09cb4884a74502f4d5544255c51ad7d3
Brynjar Glimsdal <brynjar.glimsdal@gmail.com>
2020-06-07 13:46:11 +0200

    timed run

--- src/main/java/Master2020/Run/App.java
+++ src/main/java/Master2020/Run/App.java
@@ -38,6 +38,7 @@ public class App {
         initialize();
         Parameters.customFileName = "fullRun" + args[1];
         Parameters.totalRuntime = 1800000;
+        Parameters.customFileName = "-timed-for-" + args[0];
         for (int iteration = 0 ; iteration < 1 ; iteration++){
             for (int dataset = 0 ; dataset < 2 ; dataset++){
                 Parameters.useVestTeleDataset = dataset == 0;
@@ -144,8 +145,8 @@ public class App {
         vehicles = new int[5];
         journeys = new int[5];
         //25
-        seeds[0][0] = new int[]{};//15, 84};
-        seeds[1][0] = new int[]{};//69,85};
+        seeds[0][0] = new int[]{15, 84};
+        seeds[1][0] = new int[]{69,85};
         customers[0] = 25;
         vehicles[0] = 12;
         journeys[0] = 3;
@@ -156,19 +157,19 @@ public class App {
         vehicles[1] = 25;
         journeys[1] = 3;
         //75
-        seeds[0][2] = new int[]{32,18,37};
-        seeds[1][2] = new int[]{1};
+        seeds[0][2] = new int[]{};//32,18,37};
+        seeds[1][2] = new int[]{};//1};
         customers[2] = 75;
         vehicles[2] = 32;
         journeys[2] = 2;
         //100
-        seeds[0][3] = new int[]{97,46,35,76};
+        seeds[0][3] = new int[]{};//97,46,35,76};
         seeds[1][3] = new int[]{};
         customers[3] = 100;
         vehicles[3] = 50;
         journeys[3] = 2;
         //115
-        seeds[0][4] = new int[]{10,27};
+        seeds[0][4] = new int[]{};//10,27};
         seeds[1][4] = new int[]{};
         customers[4] = 115;
         vehicles[4] = 62;
