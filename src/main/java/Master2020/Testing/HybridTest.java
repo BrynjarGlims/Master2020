b5df51994b1d15a38113a30f2c97155d5e5270fe
LarsTv <lars-tvinnereim@hotmail.com>
2020-06-07 11:28:45 +0200

    change in prints

--- src/main/java/Master2020/Testing/HybridTest.java
+++ src/main/java/Master2020/Testing/HybridTest.java
@@ -33,24 +33,22 @@ public class HybridTest {
 
     public static boolean checkIfJourneysExists(ArrayList<Journey>[][] journeys, Data data, int counter){
         boolean journeyExists = false;
-        System.out.print("Number of journeys generated for ");
         int count = 0;
         Origin id = null;
         for (int p = 0; p < data.numberOfPeriods; p++){
-            if (journeys[p][0].size() > 0) {
-                id = journeys[p][0].get(0).ID;
-                journeyExists = true;
-            }
             for (int vt = 0; vt < data.numberOfVehicleTypes; vt++){
+                if (journeys[p][vt].size() > 0) {
+                    id = journeys[p][vt].get(0).ID;
+                    journeyExists = true;
+                }
                 for (Journey journey: journeys[p][vt]){
                     count += 1;
                 }
             }
         }
         if(id == null){
-            System.out.println("No id found for algorithm " + counter);
+            System.out.println("No id found for algorithm " + counter + " number of journeys " + count);
         }
-        System.out.println(id.toString() + ": " + count );
         return journeyExists;
     }
 }
